4a87e4e4af61c4d7ded99836039f9c4534899950
7csm <bpawan3370@gmail.com>
2024-11-24 23:39:06 +0530

    exp8

---
+++ exp8/src/app/app.component.ts
@@ -0,0 +1,16 @@
+ 
+import { Component } from '@angular/core';
+import { FetchDataService } from './fetch-data.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'exp8_reading_data_from_server';
+  userList:any[] = []; 
+  constructor(private fetchService: FetchDataService) {} 
+  ngOnInit(): void{ 
+    this.fetchService.getUser().subscribe(user => this.userList = user); 
+  }
+}
